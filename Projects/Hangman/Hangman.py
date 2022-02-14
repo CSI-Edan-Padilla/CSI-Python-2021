9f96c472b5b1c459361b32c53087bc77b638e6d0
CSI-Edan-Padilla <89210538+CSI-Edan-Padilla@users.noreply.github.com>
2022-02-14 11:57:26 -0400

    hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,3 +1,15 @@
+import json, ssl
+import random
+import urllib. request
+from randomname import Name 
+
+def getWord():
+
+    nameURL = "https://random-data-api.com/api/name/random_name"
+
+    request = urllib.request.Request(nameURL)
+    requestURL = json.loads(urllib.request.urlopen(request))
+
 steps = ["""
         __________________
         |                 |
@@ -120,4 +132,3 @@ steps = ["""
         """]
 
 
-        
