dbf596b00356bd851e58bc29e6e2fe0e6c82f111
CSI-Edan-Padilla <89210538+CSI-Edan-Padilla@users.noreply.github.com>
2022-03-01 11:49:54 -0400

    hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,19 +1,28 @@
+
 import json, ssl
+
 import random
 import urllib. request
 from randomname import Name 
 
 def getWord():
 
+
+    
+    ssl._create_default_https_context = ssl._create_unverified_context
+
     nameURL = "https://random-data-api.com/api/name/random_name"
 
     request = urllib.request.Request(nameURL)
 
-    requestURL = json.loads(urllib.request.urlopen(request))
+    requestURL = json.loads(urllib.request.urlopen(request).read())
 
     current_name = Name(**requestURL)
 
-    return current_name.name
+    return current_name.name.upper()
+
+   
+myWord = getWord()
 
 
 steps = ["""
@@ -137,7 +146,7 @@ steps = ["""
         |
         """]
 
-print(steps[0])
+# print(steps[0])
 
 wrong_numbers = ["0","1","2","3","4","5","6","7","8","9"]
 wrong_symbols = ["`","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","\\","|",":",";","'","<",">",",",".","?","/"]
@@ -147,42 +156,49 @@ def getInput():
     while(True):
         letter =  input( "Try to find the word to win the game of Hangman by finding the letters: " )
 
-        if (len,(letter) !=1):
+        if (len(letter) !=1):
             continue 
 
         if letter in wrong_symbols:
             print("Wrong, you are bad 👎🏽")
             continue 
 
-        if letter in wrong_symbols:
-            print("Again, why are you bad?😐")
-            continue 
+        # if letter in wrong_symbols:
+        #     print("Again, why are you bad?😐")
+        #     continue 
 
         if letter in attemptedletters:
             print("Bad 💩")
             continue 
 
         attemptedletters.append(letter)
-        return letter
+        return letter.upper()
 
 
 def printword():
     temp:str=""
-    for letter in Name:
-        if letter in Name:
-            print(letter)
-            letter in attemptedletters
-    if letter not in attemptedletters:
-        temp +="_"
-    else:
-        temp += letter
+    for letter in myWord:
+        if letter in attemptedletters:
+             temp += letter
+        else:
+            temp +="_"
     return temp
+
+def printsteps():
+    phases = 0
+    for letter in attemptedletters:
+        if letter not in myWord:
+            phases = phases +1
+    print(steps[phases])
 
 
 while True:
-    print(steps[0])
+    printsteps()
+    print(printword())
     getInput()
-    print()
+  
+
+
     
 
 
