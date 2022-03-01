a5bbb48d0a4b1377582d9a1887b2ef71a8972744
CSI-Edan-Padilla <89210538+CSI-Edan-Padilla@users.noreply.github.com>
2022-03-01 12:02:04 -0400

    handmang.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -23,7 +23,7 @@ def getWord():
 
    
 myWord = getWord()
-
+print(myWord)
 
 steps = ["""
         __________________
@@ -149,7 +149,8 @@ steps = ["""
 # print(steps[0])
 
 wrong_numbers = ["0","1","2","3","4","5","6","7","8","9"]
-wrong_symbols = ["`","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","\\","|",":",";","'","<",">",",",".","?","/"]
+wrong_symbols = ["`","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","\\","|",":",";","'","<",">",",",".","?","/",""] 
+
 attemptedletters = []
 
 def getInput():
@@ -171,7 +172,7 @@ def getInput():
             print("Bad 💩")
             continue 
 
-        attemptedletters.append(letter)
+        attemptedletters.append(letter.upper())
         return letter.upper()
 
 
